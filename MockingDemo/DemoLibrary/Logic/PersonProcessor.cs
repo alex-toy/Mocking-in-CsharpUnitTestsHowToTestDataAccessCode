bb8af5da22a26ee01b4639ec638bce2322851d7e
alex-toy <alexei.80@hotmail.fr>
2023-02-25 09:47:49 +0100

    cleaning

---
+++ MockingDemo/DemoLibrary/Logic/PersonProcessor.cs
@@ -0,0 +1,108 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace DemoLibrary.Logic
+{
+    public class PersonProcessor : IPersonProcessor
+    {
+        ISqliteDataAccess _database;
+
+        public PersonProcessor(ISqliteDataAccess database)
+        {
+            _database = database;
+        }
+
+        public PersonModel CreatePerson(string firstName, string lastName, string heightText)
+        {
+            CheckPersonParameters(firstName, lastName, heightText);
+
+            PersonModel personModel = GetPersonModel(firstName, lastName, heightText);
+
+            return personModel;
+        }
+
+        private PersonModel GetPersonModel(string firstName, string lastName, string heightText)
+        {
+            PersonModel personModel = new PersonModel();
+            personModel.FirstName = firstName;
+            personModel.LastName = lastName;
+            double heightInInches = ConvertHeightTextToInches(heightText).heightInInches;
+            personModel.HeightInInches = heightInInches;
+            return personModel;
+        }
+
+        private void CheckPersonParameters(string firstName, string lastName, string heightText)
+        {
+            if (!IsValidName(firstName)) throw new ArgumentException("The value was not valid", "firstName");
+            if (!IsValidName(lastName)) throw new ArgumentException("The value was not valid", "lastName");
+            var height = ConvertHeightTextToInches(heightText);
+            if (!height.isValid) throw new ArgumentException("The value was not valid", "heightText");
+        }
+
+        public List<PersonModel> LoadPeople()
+        {
+            string sql = "select * from Person";
+
+            return _database.LoadData<PersonModel>(sql);
+        }
+
+        public void SavePerson(PersonModel person)
+        {
+            string sql = "insert into Person (FirstName, LastName, HeightInInches) " +
+                "values (@FirstName, @LastName, @HeightInInches)";
+            
+            _database.SaveData(person, sql);
+        }
+
+        public void UpdatePerson(PersonModel person)
+        {
+            string sql = "update Person set FirstName = @FirstName, LastName = @LastName" +
+                ", HeightInInches = @HeightInInches where Id = @Id";
+            
+            _database.UpdateData(person, sql);
+        }
+
+        public (bool isValid, double heightInInches) ConvertHeightTextToInches(string heightText)
+        {
+            bool correctFormatHeight = CheckHeightFormat(heightText);
+
+            if (!correctFormatHeight) return (false, 0);
+
+            string[] heightParts = heightText.Split(new char[] { '\'', '"' });
+            bool hasFeetPart = int.TryParse(heightParts[0], out int feet) == true;
+            bool hasInchesPart = double.TryParse(heightParts[1], out double inches) == true;
+            bool hasBothFeetAndInchesPart = hasFeetPart && hasInchesPart;
+            if (!hasBothFeetAndInchesPart) return (false, 0);
+
+            double heightInInches = (feet * 12) + inches;
+
+            return (true, heightInInches);
+        }
+
+        private static bool CheckHeightFormat(string heightText)
+        {
+            int feetMarkerLocation = heightText.IndexOf('\'');
+            bool hasFeetMarker = feetMarkerLocation >= 0;
+
+            int inchesMarkerLocation = heightText.IndexOf('"');
+            bool hasInchesMarker = inchesMarkerLocation >= 0;
+
+            bool correctOrderMarkers = inchesMarkerLocation >= feetMarkerLocation;
+
+            bool correctFormatHeight = hasFeetMarker && hasInchesMarker && correctOrderMarkers;
+            return correctFormatHeight;
+        }
+
+        private bool IsValidName(string name)
+        {
+            char[] invalidCharacters = "`~!@#$%^&*()_+=0123456789<>,.?/\\|{}[]'\"".ToCharArray();
+            bool nameOnlyCorrectCharacters = name.IndexOfAny(invalidCharacters) < 0;
+
+            bool nameCorrectLength = name.Length >= 2;
+
+            return nameOnlyCorrectCharacters && nameCorrectLength;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mocking in C# Unit Tests How To Test Data Access Code
+
+## Install packages
+```
+Moq
+Autofac.Extras.Moq
+```
+
+<img src="/pictures/test_project.png" title="upload"  width="400">
